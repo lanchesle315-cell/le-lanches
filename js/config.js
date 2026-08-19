a9f732f97362b803a08be56ec5fd9e30527f37fb
lanchesle315-cell <lanches.le315@gmail.com>
2026-08-19 15:58:19 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,8 @@
 window.APP_CONFIG = {
   supabaseUrl: "https://xkpwudryhqgjazbpwqwb.supabase.co",
   supabaseAnonKey: "sb_publishable_ddaRsLHPWSvH29VjvowWOA_mg1wpKpR",
-  googleMapsApiKey: "AIzaSyBLA7ZbAj4y0fQYY4kbfi_mcZAmw-LcZmg",
+  //googleMapsApiKey: "AIzaSyBLA7ZbAj4y0fQYY4kbfi_mcZAmw-LcZmg",
+  googleMapsApiKey: "AIzaSyAnnRYERT-gHuAvHMyvOBKHXoGH9IzLaJM",
   whatsappNumber: "5515996314700",
   storeName: "Lê Lanches",
   storeAddress: "R. Firmino Mineli, 315 - Jardim Hungares, Sorocaba - SP, 18075-700",
